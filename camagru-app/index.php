d2a162b1586ba90999d684799fc85c8d935f77b4
Artem Likhtorovych <alikhtorovych@integrity.com.ua>
2019-07-08 16:06:13 +0300

    separated structure

--- camagru-app/index.php
+++ camagru-app/index.php
@@ -1,45 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
-	<!-- Styling for public area -->
-	<link rel="stylesheet" href="static/css/styles.css">
-	<meta charset="UTF-8">
+	<?php require_once('includes/head_section.php') ?>
+	
 	<title>Camagru</title>
 </head>
 <body>
 	<!-- container - wraps whole page -->
 	<div class="container">
 		<!-- navbar -->
-		<div class="navbar">
-			<div class="logo_div">
-				<a href="index.php"><h1>Camagru</h1></a>
-			</div>
-			<ul>
-			  <li><a class="active" href="index.php">Home</a></li>
-			  <li><a href="#news">News</a></li>
-			  <li><a href="#contact">Contact</a></li>
-			  <li><a href="#about">About</a></li>
-			</ul>
-		</div>
-		<!-- // navbar -->
+		
+		<?php include('includes/navbar.php') ?>
 
 		<!-- Page content -->
 		<div class="content">
-			<h2 class="content-title">Recent Articles</h2>
+			<h2 class="content-title">Photos</h2>
 			<hr>
 			<!-- more content still to come here ... -->
 		</div>
 		<!-- // Page content -->
 
 		<!-- footer -->
-		<div class="footer">
-			<p>MyViewers &copy; <?php echo date('Y'); ?></p>
-		</div>
-		<!-- // footer -->
-
-	</div>
-	<!-- // container -->
-</body>
-</html>
+		<?php include('includes/footer.php') ?>
